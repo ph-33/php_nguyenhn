f73df414344beea791c72476a4b5cf6d6c109440
megamindbo <nguyennhdn@gmail.com>
2019-12-20 21:57:25 +0700

    ad

--- PHP ASSIGNMENT/câu 5/index.php
+++ PHP ASSIGNMENT/câu 5/index.php
@@ -5,7 +5,7 @@
 	<title>Document</title>
 </head>
 <body>
-	nhanh developer
+	nhanh b1
 	<?php 
 		$sum = 0;
 		for($i = 0 ; $i <= 50 ; $i++)
@@ -13,7 +13,7 @@
 			$sum += $i;
 		}
 		echo 'b1'.$sum;
-		echo 'day la branch developer';
+		echo 'day la branch b1';
 	 ?>
 </body>
 </html>
