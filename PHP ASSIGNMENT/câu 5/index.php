8bb9cad10aec9812860f4651517444a29784f79f
megamindbo <nguyennhdn@gmail.com>
2020-01-01 09:53:00 +0700

    ad

--- PHP ASSIGNMENT/câu 5/index.php
+++ PHP ASSIGNMENT/câu 5/index.php
@@ -5,7 +5,7 @@
 	<title>Document</title>
 </head>
 <body>
-	nhanh b2 featch b2n2 sua local this is b2
+	nhanhf dadsjfal b1al this is b2
 	<?php 
 		$sum = 0;
 		for($i = 0 ; $i <= 50 ; $i++)
@@ -13,7 +13,8 @@
 			$sum += $i;
 		}
 		echo 'b1'.$sum;
-		echo 'day b2 la b2 branch b2dev';
+		echo 'day b1 la b2 branch b1dev';
+		eadjfa;l
 	 ?>
 </body>
 </html>
