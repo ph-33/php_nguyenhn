e078f55d01b326186ffb460c6c30d887d85212ec
megamindbo <nguyennhdn@gmail.com>
2020-01-01 10:37:54 +0700

    them b1

--- PHP ASSIGNMENT/câu 5/index.php
+++ PHP ASSIGNMENT/câu 5/index.php
@@ -5,15 +5,15 @@
 	<title>Document</title>
 </head>
 <body>
-	nhanhf day laf b2 vao day dsafafsd
+	nhanhf day laf b2 vadfsafo day dsafafsd
 	<?php
 		$sum = 0;
 		for($i = 0 ; $i <= 50 ; $i++)
 		{
 			$sum += $i;
 		}
-		echo 'b1'.$sum;
-		echo 'day suar day la b2 lai b1 h b1dev';
+		echo 'b1sdafafdas'.$sum;dfsafd
+		echo 'day asdfsuar day la b2 lai b1 h b1dev';
 		eadjfa;l
 	 ?>
 </body>
